f5f209d1d0dc747ccade9c4d87c139c8d48a12ca
Raissa Lima <rclraissa@gmail.com>
2022-08-22 08:45:13 -0300

    feat: added account routes

--- src/routes/account.routes.ts
+++ src/routes/account.routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { validateAccountRequest } from '../middlewares/validateCpf';
+import { validateAccountRequest } from '../middlewares/validateAccountRequest';
 import { body } from 'express-validator';
 import { AccountController } from '../controllers/account.controller';
 import { container } from 'tsyringe';
@@ -19,10 +19,26 @@ userRoutes.post('/', (req, res) => {
   return accountController.createAccount(req, res);
 });
 
-userRoutes.get('/:account', validateAccountRequest, (req, res) => {
+userRoutes.get('/', validateAccountRequest, (req, res) => {
+  return accountController.getAllAccounts(req, res);
+});
+
+userRoutes.get('/:id', validateAccountRequest, (req, res) => {
   return accountController.getAccount(req, res);
 });
 
-userRoutes.get('/all', (req, res) => {
-  return accountController.getAllAccounts(req, res);
+userRoutes.post('/deposit', validateAccountRequest, (req, res) => {
+  return accountController.depositMoney(req, res);
 });
+
+userRoutes.post('/withdraw', validateAccountRequest, (req, res) => {
+  return accountController.withdrawMoney(req, res);
+});
+
+userRoutes.post('/transfer', validateAccountRequest, (req, res) => {
+  return accountController.transferMoney(req, res);
+});
+
+userRoutes.post('/update', validateAccountRequest, (req, res) => {
+  return accountController.updateAccount(req, res);
+})
